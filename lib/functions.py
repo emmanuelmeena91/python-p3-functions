7125363f863ccea314f0aa84f476f7c95ee7c48a
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-08-16 16:22:09 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    print("Hello programmer")
+    print("Hello, programmer!")
 
 def greet(name):
-    print("Hello," + name)
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print("Hello," + name)
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    return num1 + num2 
-sum = add(2 , 3)
- 
+    return num1 + num2
 
+sum_result = add(2, 3)
 
 def halve(number):
     if not isinstance(number, (int, float)):
         return 'halve two'
-    return number/ 2
-result = halve(4)
-result = halve("not NO")
+    return number / 2
 
+result1 = halve(4)
+result2 = halve("not NO")
 
 greet_programmer()
-
-greet("Naureen!")
-
-greet_with_default("Sunny!")
+greet("Guido")
+greet_with_default("Guido")
 greet_with_default()
 
-print(sum)   
-
-print(result)
-
+print(sum_result)
+print(result1)
+print(result2)
